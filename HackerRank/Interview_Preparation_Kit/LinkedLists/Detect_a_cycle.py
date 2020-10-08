a122e6cec5d65cfced5dd2738d64129199358286
spikeeSakshu <spikee.sakshu@gmail.com>
2020-10-09 01:51:05 +0530

    Cycle in LinkedList :tada:

---
+++ HackerRank/Interview_Preparation_Kit/LinkedLists/Detect_a_cycle.py
@@ -0,0 +1,27 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+Link: https://www.hackerrank.com/challenges/ctci-linked-list-cycle/problem
+
+
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+
+def has_cycle(head):
+    nodes= set()
+    
+    while head:
+        if head in nodes:
+            return 1
+        else:
+            nodes.add(head)
+            head= head.next
+    return 0
+        
+    pass
